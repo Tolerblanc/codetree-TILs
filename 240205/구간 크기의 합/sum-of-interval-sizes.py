b40cfaf6265403231b67da216450de40df472820
Hyunjun KIM <hihj070914@icloud.com>
2024-02-05 20:35:08 +0900

    Title: 구간 크기의 합, Time: 1132ms, Memory: 48MB, Status: Passed - CodeTree

---
+++ 240205/구간 크기의 합/sum-of-interval-sizes.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+points = []
+for i in range(n):
+    a, b = map(int, input().split())
+    points.append((a, 1, i))
+    points.append((b, -1, i))
+points.sort()
+
+checker = set()
+answer = 0
+for x, v, i in points:
+    if v == 1:
+        if not checker:
+            prev = x
+        checker.add(i)
+    else:
+        checker.remove(i)
+        if not checker:
+            answer += x - prev
+print(answer)
