a37c4f2fa141f5508038f8f332340a50f7c62952
Hyunjun KIM <hihj070914@icloud.com>
2024-02-07 22:15:12 +0900

    Title: 선분 바라보기 2, Time: 835ms, Memory: 41MB, Status: Passed - CodeTree

--- 240207/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ 240207/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -13,12 +13,12 @@ points.sort()
 check = set() # 인덱스 셋
 height = [] # (y, i) 힙, 0번이 지금 맨 앞에 있는 선분
 answer = 0
+visited = [False] * n
 for x, v, y, i in points:
     if v == 1: # 시작점이면
-        if not check: # 기존에 시작한 선분이 없다면
+        if not check or (height and height[0][0] > y): # 기존에 시작한 선분이 없다면
             answer += 1
-        elif height and height[0][0] > y: # 지금 선분이 더 가까이 있다면
-            answer += 1
+            visited[i] = True
         heapq.heappush(height, (y, i)) # 힙에 추가
         check.add(i)
 
@@ -28,6 +28,8 @@ for x, v, y, i in points:
             heapq.heappop(height)
             while height and height[0][1] not in check: # 맨 앞에 있는 선분이 check에 없으면 제거:
                 heapq.heappop(height)
-            answer += 1
+            if height and not visited[height[0][1]]:
+                answer += 1
+                visited[height[0][1]] = True
 
-print(answer - 1) # 맨 마지막에 체크된건 제거해줘야함
+print(answer) # 맨 마지막에 체크된건 제거해줘야함
