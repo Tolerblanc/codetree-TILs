1eb72d8e35a6c8f8b48c93c3489c3921654c519f
Hyunjun KIM <hihj070914@icloud.com>
2024-02-08 20:06:14 +0900

    Title: 선분 바라보기 2, Time: 40ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
+++ 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int N;
 
-map<int,vector<pair<int,int>>> lines;
+vector<pair<int,int>> lines;
 
 vector<vector<int>> getInput() {
     cin >> N;
@@ -33,30 +33,14 @@ int main() {
     // cout << "\n==================\n";
     
     for(int i = 0; i < N; ++i) {
-        cur_y = input[i][0] - 1;
-        for(int y = 1; y <= cur_y; ++y) {
-            for(auto it = lines[y].begin(); it != lines[y].end(); ++it) {
-                // cout << "===========start ====" << it->first << " "<<it->second << "\n";
-                // cout <<input[i][0] << " " << input[i][1] << " " <<input [i][2] << "\n";
-                if (input[i][1] >= it->first && input[i][1] <= it->second)
-                    input[i][1] = it->second;
-                // cout <<input[i][0] << " " << input[i][1] << " " <<input [i][2] << "\n";
-                if (input[1][2] >= it->first && input[1][2] <it->second)
-                    input[1][2] = it->first;
-                // cout <<input[i][0] << " " << input[i][1] << " " <<input [i][2] << "\n";
-                if (input[i][1] < it->first)
-                    break;
-            }
+        for(auto it = lines.begin(); it != lines.end(); ++it) {
+            if (input[i][1] >= it->first && input[i][1] <= it->second)
+                input[i][1] = it->second;
+            if (input[i][2] >= it->first && input[i][2] <it->second)
+                input[i][2] = it->first;
         }
-        if (input[i][1] < input[i][2])
-            lines[cur_y + 1].push_back({input[i][1], input[i][2]});
+        if(input[i][1] < input[i][2])
+            lines.push_back({input[i][1],input[i][2]});
     }
-    for(auto it = lines.begin(); it != lines.end(); ++it) {
-        // cout << it->first << ": ";
-        // for(auto vit = it->second.begin(); vit != it->second.end(); ++vit)
-        //     cout << vit->first << " " <<vit->second <<", ";
-        // cout <<"\n";
-        ans+=it->second.size();
-    }
-    cout << ans;
+    cout << lines.size();
 }
