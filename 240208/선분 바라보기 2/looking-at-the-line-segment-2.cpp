72f1cea20f3ed9e2903955c71330fbc07511a48d
Hyunjun KIM <hihj070914@icloud.com>
2024-02-08 21:22:20 +0900

    Title: 선분 바라보기 2, Time: 82ms, Memory: 2MB, Status: Passed - CodeTree

--- 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
+++ 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int N;
 
-vector<pair<int,int>> lines;
+vector<vector<int>> lines;
+vector<int> ys;
 
 vector<vector<int>> getInput() {
     cin >> N;
@@ -23,6 +24,12 @@ bool sortFunc(vector<int> vec1, vector<int> vec2) {
     return vec1[1] < vec2[1];
 }
 
+bool sortFuncRev(vector<int> vec1, vector<int> vec2) {
+    // if(vec1[2] != vec2[2])
+    //     return vec1[2] < vec2[2];
+    return vec1[0] < vec2[0];
+}
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
@@ -33,16 +40,37 @@ int main() {
     // cout << "\n==================\n";
     
     for(int i = 0; i < N; ++i) {
+        auto a = input[i];
         for(auto it = lines.begin(); it != lines.end(); ++it) {
-            if (input[i][1] >= it->first && input[i][1] <= it->second)
-                input[i][1] = it->second;
-            if (input[i][2] >= it->first && input[i][2] <= it->second)
-                input[i][2] = it->first;
-            if(input[i][1] >= input[i][2])
+            if (input[i][1] >= (*it)[0] && input[i][1] <= (*it)[1])
+                input[i][1] = (*it)[1];
+            if (input[i][2] >= (*it)[0] && input[i][2] <= (*it)[1])
+                input[i][2] = (*it)[0];
+            if (input[i][1] >= input[i][2])
                 break;
+            if (input[i][1] == (*it)[1])
+                (*it)[1] = input[i][2];
+            if (input[i][2] == (*it)[0])
+                (*it)[0] = input[i][1];
         }
-        if(input[i][1] < input[i][2])
-            lines.push_back({input[i][1],input[i][2]});
+        if(input[i][1] < input[i][2]){
+            // cout << a[1] << " " << a[2] << " " << a[0] <<"\n";
+            // cout << input[i][1] << " " << input[i][2] << " " << input[i][0] <<"\n";
+            // cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n";
+            lines.push_back({input[i][1],input[i][2], input[i][0]});
+            // cout<<lines.size() <<"\n";
+            // for(int i = 0; i < lines.size(); ++i) {
+            //     cout << lines[i][0] << " " << lines[i][1] << "\n";
+            // }
+            // cout << "\n\n==============\n\n";
+            // ys.push_back(input[i][0]);
+        }
     }
+    // cout << "\n========================\n";
+    // sort(lines.begin(),lines.end(),sortFuncRev);
+    // cur_y = lines[0][2];
+    // for(int i = 0; i < lines.size(); ++i) {
+    //     cout << lines[i][0] << " " << lines[i][1] << " " << lines[i][2] << "\n";
+    // }
     cout << lines.size();
 }
