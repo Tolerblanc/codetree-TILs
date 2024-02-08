ca5b24309e36e0b5dc50d2dc1d28bbe7ecedafec
Hyunjun KIM <hihj070914@icloud.com>
2024-02-08 21:23:51 +0900

    Title: 선분 바라보기 2, Time: 102ms, Memory: 2MB, Status: Passed - CodeTree

--- 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
+++ 240208/선분 바라보기 2/looking-at-the-line-segment-2.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int N;
 
 vector<vector<int>> lines;
-vector<int> ys;
 
 vector<vector<int>> getInput() {
     cin >> N;
@@ -24,20 +23,11 @@ bool sortFunc(vector<int> vec1, vector<int> vec2) {
     return vec1[1] < vec2[1];
 }
 
-bool sortFuncRev(vector<int> vec1, vector<int> vec2) {
-    // if(vec1[2] != vec2[2])
-    //     return vec1[2] < vec2[2];
-    return vec1[0] < vec2[0];
-}
-
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     vector<vector<int>> input = getInput();
     sort(input.begin(), input.end(), sortFunc);
-    int ans = 0;
-    int cur_y = 0;
-    // cout << "\n==================\n";
     
     for(int i = 0; i < N; ++i) {
         auto a = input[i];
@@ -54,23 +44,8 @@ int main() {
                 (*it)[0] = input[i][1];
         }
         if(input[i][1] < input[i][2]){
-            // cout << a[1] << " " << a[2] << " " << a[0] <<"\n";
-            // cout << input[i][1] << " " << input[i][2] << " " << input[i][0] <<"\n";
-            // cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n";
             lines.push_back({input[i][1],input[i][2], input[i][0]});
-            // cout<<lines.size() <<"\n";
-            // for(int i = 0; i < lines.size(); ++i) {
-            //     cout << lines[i][0] << " " << lines[i][1] << "\n";
-            // }
-            // cout << "\n\n==============\n\n";
-            // ys.push_back(input[i][0]);
         }
     }
-    // cout << "\n========================\n";
-    // sort(lines.begin(),lines.end(),sortFuncRev);
-    // cur_y = lines[0][2];
-    // for(int i = 0; i < lines.size(); ++i) {
-    //     cout << lines[i][0] << " " << lines[i][1] << " " << lines[i][2] << "\n";
-    // }
     cout << lines.size();
 }
