c3375db23800267bedf276ae11dc105302440381
Hyunjun KIM <hihj070914@icloud.com>
2024-02-11 22:04:16 +0900

    Title: 선분 위의 점, Time: 318ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240211/선분 위의 점/point-on-the-line-segment.py
@@ -0,0 +1,11 @@
+import sys
+from bisect import bisect_left, bisect_right
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+points = list(map(int, input().split()))
+points.sort()
+
+for _ in range(m):
+    start, end = map(int, input().split())
+    print(bisect_right(points, end) - bisect_left(points, start))
