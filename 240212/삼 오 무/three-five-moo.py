33dc1814a5842d0709730ea717a9e69c58a62bb5
Hyunjun KIM <hihj070914@icloud.com>
2024-02-12 19:06:56 +0900

    Title: 삼 오 무, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/삼 오 무/three-five-moo.py
+++ 240212/삼 오 무/three-five-moo.py
@@ -3,8 +3,8 @@ input = sys.stdin.readline
 
 n = int(input())
 
-answer = 1 << 31 - 1
-left, right = 0, 1 << 31 - 1
+answer = 1 << 31
+left, right = 0, 1 << 31
 while left <= right:
     mid = (left + right) // 2 # 1 ~ mid 안쪽에 숫자가 몇 개 있는가?
     cnt = mid - (mid // 3) - (mid // 5) + (mid // 15)
