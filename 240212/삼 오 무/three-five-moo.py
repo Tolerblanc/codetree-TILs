33ef19bf324427ed48b41e849bfe84e6765d269a
Hyunjun KIM <hihj070914@icloud.com>
2024-02-12 19:03:57 +0900

    Title: 삼 오 무, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240212/삼 오 무/three-five-moo.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+answer = 101010101
+left, right = 0, int(1e9)
+while left <= right:
+    mid = (left + right) // 2 # 1 ~ mid 안쪽에 숫자가 몇 개 있는가?
+    cnt = mid - (mid // 3) - (mid // 5) + (mid // 15)
+    if cnt >= n:
+        answer = min(answer, mid)
+        right = mid - 1
+    else:
+        left = mid + 1
+print(answer)
