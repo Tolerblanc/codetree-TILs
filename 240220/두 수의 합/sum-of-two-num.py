359333579a38f3f0f14d19f26d3a1301e907f220
Hyunjun KIM <hihj070914@icloud.com>
2024-02-20 23:13:52 +0900

    Title: 두 수의 합, Time: 274ms, Memory: 39MB, Status: Passed - CodeTree

--- 240220/두 수의 합/sum-of-two-num.py
+++ 240220/두 수의 합/sum-of-two-num.py
@@ -7,5 +7,8 @@ cc = Counter(list(map(int, input().split())))
 
 answer = 0
 for num in cc.keys():
+    if num == k - num:
+        answer += cc[num] * (cc[k - num] - 1)
+        continue
     answer += cc[num] * cc[k - num]
 print(answer // 2)
