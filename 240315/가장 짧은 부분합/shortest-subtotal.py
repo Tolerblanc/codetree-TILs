c93494d03434a78b55daf45fa2785f8096531e8d
Hyunjun KIM <hihj070914@icloud.com>
2024-03-15 17:38:46 +0900

    Title: 가장 짧은 부분합, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/가장 짧은 부분합/shortest-subtotal.py
+++ 240315/가장 짧은 부분합/shortest-subtotal.py
@@ -8,7 +8,7 @@ r = 0
 prefix = 0
 ans = 4242424242
 for l in range(len(nums)):
-    while r < len(nums) and prefix + nums[r] < s:
+    while r < len(nums) and prefix < s:
         prefix += nums[r]
         r += 1
     ans = min(ans, r - l + 1)
