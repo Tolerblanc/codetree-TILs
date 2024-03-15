c6c37dbd02af3ff5f952a7e025f97c08eb84d5fc
Hyunjun KIM <hihj070914@icloud.com>
2024-03-15 17:27:47 +0900

    Title: 가장 짧은 부분합, Time: 139ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 240315/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+
+r = 0
+prefix = 0
+ans = 4242424242
+for l in range(len(nums)):
+    while r < len(nums) and prefix + nums[r] < s:
+        prefix += nums[r]
+        r += 1
+    ans = min(ans, r - l + 1)
+    prefix -= nums[l]
+print(ans)
